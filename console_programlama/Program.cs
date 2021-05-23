440c1103a650eff78096634e39a9619dea1a4400
albatros-21 <05170000782@ogrenci.ege.edu.tr>
2021-05-24 01:04:17 +0300

    first commit

---
+++ console_programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin:");
+            string surName = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surName);
+        }
+    }
+}
