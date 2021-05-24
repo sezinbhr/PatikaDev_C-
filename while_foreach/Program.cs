e5949d7de48eed057a0d59f09f0148c99f5325eb
sezinbhr <bahar.sezin@yahoo.com>
2021-05-25 01:39:23 +0300

    first commit

--- while_foreach/Program.cs
+++ while_foreach/Program.cs
@@ -6,7 +6,36 @@ namespace while_foreach
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //While 
+            //Starting from 1 compute the average until the given number
+            Console.WriteLine("Enter a number:");
+            int counter = int.Parse(Console.ReadLine());
+            int i = 1;
+            int sum = 0;
+            while (i <= counter)
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine("The average is:" + sum / counter);
+
+            //Starting from 'a' to 'z' write all the letters to the console
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+
+            }
+
+            //Foreach
+            string[] cars = { "BMW", "Audi", "Porsche" };
+
+            foreach (var car in cars) //var is a generic type 
+            {
+                Console.WriteLine(car);
+            }
         }
     }
 }
