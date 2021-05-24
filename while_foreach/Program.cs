e0f165fd310791559b53e53f7a477528635f349f
sezinbhr <bahar.sezin@yahoo.com>
2021-05-25 01:19:16 +0300

    first commit

---
+++ while_foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
