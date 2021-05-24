dc0c49914b4420475e224e3fe0a7abd39515d7a4
sezinbhr <bahar.sezin@yahoo.com>
2021-05-24 22:11:37 +0300

    first commit

---
+++ debugging/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try //the code in the try block may be cause an error
+            {
+                Console.WriteLine("Enter a number:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The number is:" + sayi);
+
+            }
+
+            catch (Exception ex) // Let's catch the error, and show/fix what's happened!
+            {
+                Console.WriteLine("Error:" + ex.Message.ToString());
+
+            }
+            //finally // No matter what, I want to run the code which is written in the finally block. It's not mandatory to use
+            //{
+            //    Console.WriteLine("It's done!");
+
+            //}
+
+            try
+            {
+                // int a = int.Parse(null);
+                //int b = int.Parse("test");
+                int c = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+
+            {
+
+                Console.WriteLine("You entered a null value!");
+                Console.WriteLine(ex);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Data type couldn't matched!");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("You entered a value too big, or too small!");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("It's done successfully!");
+            }
+
+        }
+    }
+}
