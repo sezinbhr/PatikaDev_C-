c59a70af3ea096997d066681b091c8366db428eb
sezinbhr <bahar.sezin@yahoo.com>
2021-05-24 20:53:25 +0300

    first commit

--- if_else_ternary/Program.cs
+++ if_else_ternary/Program.cs
@@ -6,7 +6,31 @@ namespace if_else_ternary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Good morning!");
+            }
+
+            else if (time <= 18)
+            {
+                Console.WriteLine("Have a nice day!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+            }
+
+            string sonuc = time <= 18 ? "Have a nice day!" : "Good Night!"; //if statement shortcut version
+            sonuc = time >= 6 && time < 11 ? "Good Morning!" : time <= 18 ? "Have a nice day!" : "Good Night!";
+
+            Console.WriteLine(sonuc);
+
+
+
+
+
         }
     }
 }
