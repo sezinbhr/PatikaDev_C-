238ab5c9ef10a0bef54358746b83f59cb7e00c54
sezinbhr <bahar.sezin@yahoo.com>
2021-05-25 14:28:10 +0300

    first commit

--- arrayclass_methods/Program.cs
+++ arrayclass_methods/Program.cs
@@ -6,7 +6,57 @@ namespace arrayclass_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sort
+            int[] nums = { 23, 12, 86, 72, 3, 11, 17 };
+
+            Console.WriteLine("Unsorted array");
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Sorted array");
+            Array.Sort(nums);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Clear
+            Console.WriteLine("Array Clear");
+            Array.Clear(nums, 2, 2); //start from 2nd array element and count 2 elements from this index and replace the value with 0
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Reverse
+            Console.WriteLine("Reverse");
+            Array.Reverse(nums);
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            //IndexOf
+            Console.WriteLine("IndexOf");
+            System.Console.WriteLine(Array.IndexOf(nums, 23));
+
+            //Resize, adjust the array size
+            Console.WriteLine("Resize");
+            Array.Resize<int>(ref nums, 9);
+            nums[8] = 99;
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
         }
     }
 }
