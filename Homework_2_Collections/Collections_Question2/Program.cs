0edf995be7f57650aec4e2ed93624f13bae29008
sezinbhr <bahar.sezin@yahoo.com>
2021-06-02 19:55:21 +0300

    first commit

---
+++ Homework_2_Collections/Collections_Question2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 20 numbers:");
+            int[] numList = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Enter the value of {0}. number of the word:", i + 1);
+                numList[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numList);
+            int[] minList = numList[0..3];
+
+            Array.Reverse(numList);
+            int[] maxList = numList[0..3];
+
+
+        }
+    }
+}
